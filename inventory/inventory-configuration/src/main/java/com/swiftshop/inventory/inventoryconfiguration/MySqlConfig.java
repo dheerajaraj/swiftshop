6ad2e32d82f0623b5d98a16f5358126727cbd723
dheerajaraj <dheerajaraj@yahoo.com.sg>
2023-11-20 01:28:21 +0800

    mysql config done

---
+++ inventory/inventory-configuration/src/main/java/com/swiftshop/inventory/inventoryconfiguration/MySqlConfig.java
@@ -0,0 +1,71 @@
+package com.swiftshop.inventory.inventoryconfiguration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Component
+@MapperScan(basePackages = "com.shopee.idata.dataquality.mapper",
+        sqlSessionTemplateRef = "mysqlSqlSessionTemplate")
+@EnableTransactionManagement
+public class MySqlConfig {
+
+    @Value(value = "${jdbc.mysql.driver}")
+    private String driverName;
+
+    @Value(value = "${jdbc.mysql.url}")
+    private String mysqlUrl;
+
+    @Value(value = "${jdbc.mysql.username}")
+    private String mysqlUserName;
+
+    @Value(value = "${jdbc.mysql.password}")
+    private String mysqlPassword;
+
+    @Value(value = "${jdbc.mysql.pool.name}")
+    private String poolName;
+
+    @Bean(name = "mysqlDataSource")
+    @Primary
+    public DataSource mysqlDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driverName);
+        config.setJdbcUrl(mysqlUrl);
+        config.setUsername(mysqlUserName);
+        config.setPassword(mysqlPassword);
+        config.setPoolName(poolName);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        return new HikariDataSource(config);
+    }
+
+    @Bean(name = "mysqlSqlSessionFactory")
+    @Primary
+    public SqlSessionFactory mysqlSqlSessionFactory(
+            @Qualifier("mysqlDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean(name = "mysqlSqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate mysqlSqlSessionTemplate(
+            @Qualifier("mysqlSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
+
---
+++ inventory/inventory-configuration/src/test/java/com/swiftshop/inventory/inventoryconfiguration/InventoryConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftshop.inventory.inventoryconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
